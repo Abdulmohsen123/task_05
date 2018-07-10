fc0b32d90395383e381c5edceeedff638bc316e0
Abdulmohsen <training.coded@gmail.com>
2018-07-10 14:49:25 +0300

    added data and completed project

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
+from .models import Restaurant
+
+def get_restaurants():
+    rest= Restaurant.objects.values()
+    print(rest)
+    return rest
 
 def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
 
 def restaurant_list(request):
+    restaurants = get_restaurants()
     context = {
-        "restaurants":,
+        "restaurants": restaurants,
     }
     return render(request, 'list.html', context)
 
 
 def restaurant_detail(request, restaurant_id):
     context = {
-        "restaurant":,
+        "restaurant": Restaurant.objects.get(pk=restaurant_id),
     }
     return render(request, 'detail.html', context)
